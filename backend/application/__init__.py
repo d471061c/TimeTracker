17bd7452d1e1d508664d38021a372d3ae6e87341
d471061c <cpastethat@gmail.com>
2020-07-16 16:59:13 +0300

    add logging and update resource location

--- backend/application/__init__.py
+++ backend/application/__init__.py
@@ -1,21 +1,29 @@
-import os
+import os, logging, sys
 
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
 
-RESOURCES = "../resources"
+RESOURCES = "resources"
 
-app = Flask(__name__, static_folder=f"{RESOURCES}/static", template_folder=f"{RESOURCES}/templates")
+app = Flask(__name__, static_folder=f"../{RESOURCES}/static", template_folder=f"../{RESOURCES}/templates")
 CORS(app)
 
 # Database Configuration
-app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{RESOURCES}/data/database.db"
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///../{RESOURCES}/data/database.db"
 app.config["SQLALCHEMY_ECHO"] = False
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 db.create_all()
+
+# Logging configuration
+logging.basicConfig(format='%(asctime)s "%(name)s" [ %(levelname)-7s ] %(message)s',
+                        datefmt='%Y-%m-%d %H:%M',
+                        filename=f"{RESOURCES}/logs/server.log",
+                        level=logging.DEBUG)
+app.logger.addHandler(logging.StreamHandler(sys.stdout))
+app.logger.setLevel(logging.DEBUG)
 
 @app.route("/")
 def homepage():
